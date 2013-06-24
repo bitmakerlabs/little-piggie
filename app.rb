b8eff77dced3bbaed4daf7ea10382d9e356127f6
Ryan Ming <ryan.ming@gmail.com>
2013-06-24 17:02:54 -0400

    extended hello world to pick from a sample of compliments

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 
+COMPLIMENTS = [
+  'Looking good! Have you lost weight?',
+  'I like your shoes.',
+  'Your pet loves you too much to ever run away.',
+  'Your sneezes sound like a chorus of angels giggling.',
+  'Your hair smells like freshly cut grass.',
+  '8 out of 10 co-workers agree your desk is the cleanest.'
+]
+
 get '/' do
-  'Looking good today!'
+  COMPLIMENTS.sample
 end
